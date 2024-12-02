156e1a4e6f53ebf6fad84b3b22dd466738b7f3c5
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-12-02 15:22:18 +0530

    first commit

---
+++ 2.Print table using While Loop(example -2).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number=2;
+    int i=1;
+    while(i<=10)
+    {
+        cout<<"2x"<<i<<"="<<number*i<<"\n";
+         ++i;
+    }
+}
