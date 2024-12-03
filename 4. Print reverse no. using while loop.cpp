88de667b4a6d6f0c6d89588beeb851675789e3df
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-12-04 02:53:17 +0530

    new code

---
+++ 4. Print reverse no. using while loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number=123;
+    int rv=0;
+    while(number!=0)
+    {
+        rv=rv*10 + number %10;
+        number =number/10;
+    }
+   cout<<rv;
+}
